ba83e517fa14fcf82d117f9bc868e9d1fc3c02d4
ChitraPatgar <65712926+ChitraPatgar@users.noreply.github.com>
2021-02-13 00:14:36 +0530

    Add files via upload

---
+++ assignemt no3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a=input("enter first string")
+b=a[::-1]
+print(a)
+print(b)
+
+
+# In[ ]:
+
+
+for i in range(0,5):
+    for j in range(0,i+1):
+          print(' *',end='')
+    print("\r")
+for i in range(5,0,-1):
+    for j in range(0,i-1):
+          print(' *',end='')
+    print("\r")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
